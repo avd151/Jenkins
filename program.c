9d06c4aab312087b0cb7b7b986d7584bb9e07cfa
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-10-30 12:10:17 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int add(int num1, int num2){
+  return (num1+num2);
+}
+
+int main(){
+  int n1 = 2, n2 = 3;
+  int sum = add(n1, n2);
+  printf("Sum of %d and %d = %d\n", n1, n2, sum);
+  return 0;
+}
