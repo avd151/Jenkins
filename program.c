792840327b52db592dd1c16d1467ea6d6a0d11c2
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-11-11 13:14:28 +0530

    added multiply function

--- program.c
+++ program.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+int multiply(int num1, int num2){
+  return (num1*num2);
+}
+
 int add(int num1, int num2){
   return (num1+num2);
 }
@@ -8,6 +12,8 @@ int main(){
 //   int n1 = 2, n2 = 3;
   int n1 = 5, n2 = 7; //changed values of numbers
   int sum = add(n1, n2);
+  int prod = multiply(n1,n2);
   printf("Sum of %d and %d = %d\n", n1, n2, sum);
+  printf("Product of %d and %d = %d\n", n1, n2, prod);
   return 0;
 }
