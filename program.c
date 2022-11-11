637e51abe290774f3ec38ff759b5a788d5e7334f
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-11-11 12:21:55 +0530

    changed variable values

--- program.c
+++ program.c
@@ -5,7 +5,8 @@ int add(int num1, int num2){
 }
 
 int main(){
-  int n1 = 2, n2 = 3;
+//   int n1 = 2, n2 = 3;
+  int n1 = 5, n2 = 7;
   int sum = add(n1, n2);
   printf("Sum of %d and %d = %d\n", n1, n2, sum);
   return 0;
