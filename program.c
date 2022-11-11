ab5bbda9aa0296eb0584a8406e672948046a1ede
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-11-11 12:39:23 +0530

    Update program.c

--- program.c
+++ program.c
@@ -6,7 +6,7 @@ int add(int num1, int num2){
 
 int main(){
 //   int n1 = 2, n2 = 3;
-  int n1 = 5, n2 = 7; //changed values
+  int n1 = 5, n2 = 7; //changed values of numbers
   int sum = add(n1, n2);
   printf("Sum of %d and %d = %d\n", n1, n2, sum);
   return 0;
